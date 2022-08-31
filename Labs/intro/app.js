d75d2f50b776d397a59b14fbd7488c83b9c11a3f
Sarah Wood <sarah.wood1459@gmail.com>
2022-08-30 22:24:16 -0400

    finished monsieur tortue! Such a handsome lad

--- Labs/intro/app.js
+++ Labs/intro/app.js
@@ -1,3 +1,22 @@
 document.write("Hello world");
 
 document.write("This is a test");
+
+
+let myName = "Sarah";
+
+myName += ", a barista";
+console.log(myName);
+
+document.write("Hello!");
+
+let num = 10;
+let b = 20;
+console.log("before");
+
+//true
+if(num == b)
+{
+console.log("during");
+}
+console.log("after");
--- Labs/intro/pc.js
+++ Labs/intro/pc.js
@@ -1,20 +1,39 @@
-function
-setup()
-{
-    createCanvas(480,120);
+var c = 200;
+var t = 10;
+var f = 15;
+
+function 
+setup() {
+  createCanvas(400, 400);
+  
 }
-
-function
-   draw()
-    {
-      if(mouseIsPressed)
-      {
-        fill(0);
-      }
-      else
-      {
-          fill(255);
-      }
-
-      ellipse(mouse, mouseY, 80, 80);
-    }
+//turtle
+// half circle, opposite of unit circle
+// complete cicle for monsieur tortue's head, tinker with coords to connect with semi circle
+//monsieur tortue needs an eye
+// what is the main x coordinate? 
+// leg day, tamper with coords
+// triangle, tamper with coords
+//color
+//polish and tidy
+function draw() 
+{
+  
+  strokeWeight(2);
+  fill(0,255,0);
+  arc(150,190,30,30, radians(0),radians(360));
+  fill(0,0,0);
+  arc(145,185,5,5,radians(0),radians(360));
+  strokeWeight(3);
+  fill(0,102,0);
+  arc(c,c,80,80,radians(180),radians(360));
+  strokeWeight(1);
+  fill(0,255,0);
+  rect(165,c,t,f);
+  rect(178,c,t,f);
+  rect(215,c,t,f);
+  rect(228,c,t,f);
+  strokeWeight(1.5);
+  strokeJoin(ROUND);
+  triangle(240,205,280,c,235,c);
+}
